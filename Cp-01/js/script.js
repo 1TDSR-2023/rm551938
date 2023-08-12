74162d4fa7c22292382c87a4c218b06783141f8c
DennerDuarte <rm551938@fiap.com.br>
2023-08-11 22:35:21 -0300

    Prova Finalizada - 6

--- Cp-01/js/script.js
+++ Cp-01/js/script.js
@@ -5,7 +5,10 @@ let listaDepartamentoArray = [];
 let listaImportanciaArray = [];
 let listaTarefasArray = [];
 
+let listaGeral= [];
+
 const botaoAddItens = document.querySelector("#btnAddItens");
+const botaoOrdenar = document.querySelector("#btnOrdenar");
 
 botaoAddItens.addEventListener("click", (evento) => {
 
@@ -14,14 +17,19 @@ botaoAddItens.addEventListener("click", (evento) => {
     const inputDescricao = document.querySelector("#idDescricao");
     const inputAutor = document.querySelector("#idAutor");
     const inputDepartamento = document.querySelector("#idDepartamento");
-    const inputImportancia = document.querySelector("#Importancia");
+    const inputImportancia = document.querySelector("#idImportancia");
     const inputTarefa = document.querySelector("#idTarefa");
+
+    let tarefa = {
+        descricao: inputDescricao.value,
+
+    }
 
     listaDescricaoArray.push(inputDescricao.value);
     listaAutorArray.push(inputAutor.value);
     listaDepartamentoArray.push(inputDepartamento.value);
     listaImportanciaArray.push(inputImportancia);
-    listaTarefasArray.push(inputTarefa);
+    listaTarefasArray.push(inputTarefa.value);
 
     const tabelaDeTarefas = document.querySelector("#tabela-tarefas");
 
@@ -30,23 +38,16 @@ botaoAddItens.addEventListener("click", (evento) => {
     const tdAutor = document.createElement("td");
     const tdDepartamento = document.createElement("td");
     const tdTarefa = document.createElement("td");
-    
-    
     const tdImporantcia = document.createElement("td");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.disabled = true;
-    tdImporantcia.appendChild(checkbox);
-
     
-    
-  
-
 
     tdDescricao.textContent = inputDescricao.value;
     tdAutor.textContent = inputAutor.value;
     tdDepartamento.textContent = inputDepartamento.value;
+    tdImporantcia.textContent = inputImportancia;
+    tdTarefa.textContent = inputTarefa.value;
 
+    tr.appendChild(tdTarefa);
     tr.appendChild(tdDescricao);
     tr.appendChild(tdAutor);
     tr.appendChild(tdDepartamento);
@@ -54,6 +55,28 @@ botaoAddItens.addEventListener("click", (evento) => {
 
     tabelaDeTarefas.appendChild(tr);
 
+   
+    let selectTempo = document.createElement("select");
+    let optionTempo1Semana = document.createElement("option");
+    let optionTempo1Ano = document.createElement("option");
+    let optionTempo10Anos = document.createElement("option");
+    
+    
+    optionTempo1Semana.textContent = "1 semana";
+    optionTempo1Ano.textContent = "1 Ano";
+    optionTempo10Anos.textContent = "10 Anos";
+
+    selectTempo.appendChild(optionTempo1Semana);
+    selectTempo.appendChild(optionTempo1Ano);
+    selectTempo.appendChild(optionTempo10Anos);
+
+    tr.appendChild(selectTempo);
+   
+
+    let valorTarefa = document.createElement("input");
+    valorTarefa.type = "txt";
+    tr.appendChild(valorTarefa);
+   
     let btnPraApagarLinha = document.createElement("button");
 
     btnPraApagarLinha.textContent = "*APAGAR LINHA*"
@@ -62,11 +85,11 @@ botaoAddItens.addEventListener("click", (evento) => {
 
     btnPraApagarLinha.addEventListener("click", (evento) =>{
 
-        let trApaga = e.target.parentNode;
+        let trApaga = evento.target.parentNode;
 
-        console.log(trApaga);
+        trApaga.remove();
+    
 
     });
-
 
 });
